25f40dc51a4986280186c2bd665e8c5991a78441
Dustin <dustin.tran@live.com>
2018-07-22 11:27:04 +0900

    completed basic requirements

--- scripts/selecting/juneProjects.sql
+++ scripts/selecting/juneProjects.sql
@@ -1,1 +1,9 @@
--- Your SQL here
+-- Your SQL here * [ ] 
+-- Select the `name` of all projects worked on by students who 
+-- checked in in June 2016. There should be no duplicates in the list
+select distinct name
+from projects
+inner join students_to_projects on projects.id = students_to_projects.project_id
+inner join students on students_to_projects.student_id = students.id
+inner join checkins on students.id = checkins.student_id
+where extract(year from checkins.checked_in_at) = 2016 and extract(month from checkins.checked_in_at) = 6;
--- scripts/selecting/studentsOnProjectX.sql
+++ scripts/selecting/studentsOnProjectX.sql
@@ -1,1 +1,5 @@
 -- Your SQL here
+select first_name, last_name
+from students
+inner join students_to_projects on students.id = students_to_projects.student_id
+where students_to_projects.project_id = 5;
